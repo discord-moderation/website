17923cc2638203a75dac7e4d656238deeacfe86c
bad-boy-discord <46648824+bad-boy-discord@users.noreply.github.com>
2021-08-19 23:28:54 +0300

    Delete assets directory

--- assets/Spinner.1a18cfd3.js
+++
@@ -1,1 +0,0 @@
-import{o as s,c as l,a as r}from"./vendor.8173c927.js";const a={},e={class:"relative h-10 w-10 my-60 mx-auto"},o=r("div",{class:"cube1 bg-discord-blurple-560 h-4 w-4 absolute"},null,-1),c=r("div",{class:"cube2 bg-discord-blurple-560 h-4 w-4 absolute"},null,-1);a.render=function(r,a){return s(),l("div",e,[o,c])};var u=a;export{u as S};
